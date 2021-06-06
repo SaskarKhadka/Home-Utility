2fa472c4c5ad7a84a4d5a4e298e29d8586f633dd
saugat poudel <saugat.poudel478@gmail.com>
2021-06-06 18:25:19 +0545

    updated user profile

--- home_utility/lib/screens/tabPages/userProfile.dart
+++ home_utility/lib/screens/tabPages/userProfile.dart
@@ -6,16 +6,139 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> {
-  @override
-
-  //TODO: Implement user profile
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Text('This is User Profile'),
+  Widget textfield({@required String hintText}) {
+    return Material(
+        elevation: 4,
+        shadowColor: Colors.grey,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
         ),
+        child: TextField(
+          decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: TextStyle(
+                letterSpacing: 2,
+                color: Colors.black54,
+                fontWeight: FontWeight.bold,
+              ),
+              fillColor: Colors.white30,
+              filled: true,
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide.none)),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Color(0xff555555),
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: 385,
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    textfield(hintText: 'Username'),
+                    textfield(hintText: 'Email'),
+                    textfield(hintText: 'Address'),
+                    textfield(hintText: 'Phonenumber'),
+                    Container(
+                        height: 50,
+                        width: 200,
+                        child: FlatButton(
+                          shape: StadiumBorder(),
+                          padding: EdgeInsets.all(0),
+                          color: Colors.black,
+                          onPressed: () {},
+                          child: Text(
+                            "Update Info",
+                            style: TextStyle(fontSize: 23, color: Colors.white),
+                          ),
+                        ))
+                  ],
+                ),
+              ),
+            ],
+          ),
+          CustomPaint(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            ),
+            painter: Header_curve_cont(),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  'PROFILE',
+                  style: TextStyle(
+                    fontSize: 35,
+                    letterSpacing: 1.5,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                width: MediaQuery.of(context).size.width / 2,
+                height: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 5),
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('images/img-3.jpg'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 270, left: 184),
+            child: CircleAvatar(
+              backgroundColor: Colors.black54,
+              child: IconButton(
+                icon: Icon(
+                  Icons.edit,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
+}
+
+class Header_curve_cont extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = Color(0xff555555);
+    Path path = Path()
+      ..relativeLineTo(0, 150)
+      ..quadraticBezierTo(size.width / 2, 225, size.width, 150)
+      ..relativeLineTo(0, -150)
+      ..close();
+    canvas.drawPath(path, paint);
+  }
+
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
