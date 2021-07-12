2bc73c0a3dd91f01e49a5b7c22eacd5c26b55871
Saugat Adhikari <saugatadhikari.sa@gmail.com>
2021-07-12 16:01:43 +0545

    new service page update

--- home_utility/lib/screens/mainScreen.dart
+++ home_utility/lib/screens/mainScreen.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/gestures.dart';
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:line_icons/line_icons.dart';
+
 
 import 'tabPages/userRequestsPage.dart';
 import 'tabPages/userProfile.dart';
